0d5974bdc1b13b2e847fa30c8c37bb5c67e01805
Lidetu Tadesse <lidetutadesse6@gmail.com>
2025-11-21 23:37:14 +0300

    admin-layout and mobilesidebar menu changed

--- componentsCreated/MobileSidebar.tsx
+++ componentsCreated/MobileSidebar.tsx
@@ -1,40 +1,43 @@
 //@ts-nocheck
-import { Link } from "react-router"
+import { Link } from "react-router";
 import NavItems from "./NavItems";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
-import NavBar from "~/routes/root/NavBar";
+// import NavBar from "~/routes/root/NavBar";
 
 const MobileSidebar = () => {
-    let sidebar: SidebarComponent;
-    const toggleSidebar = () =>{
-        sidebar.hide();
-    }
+  let sidebar: SidebarComponent;
+  const toggleSidebar = () => {
+    sidebar.hide();
+  };
   return (
     <div className="mobile-sidebar wrapper">
-        <header>
-            <Link to='/' className="logo">
-                <img src="/assets/images/Visit-kembata-logo.jpg" 
-                alt="Visit Kembata Logo"
-                className="size-[40px]" />
-                <h1>Visit Kembata</h1>
-            </Link>
-    
-            <button onClick={() => sidebar.toggle()}>
-                <img src="/assets/icons/menu.svg" alt="menu" className="size-7"/>
-            </button>
-        </header>
-        <SidebarComponent width={270}
-        ref={(Sidebar) => sidebar = Sidebar}
+      <header>
+        <Link to="/" className="logo">
+          <img
+            src="/assets/images/Visit-kembata-logo.jpg"
+            alt="Visit Kembata Logo"
+            className="size-[40px]"
+          />
+          <h1>Visit Kembata</h1>
+        </Link>
+
+        <button onClick={() => sidebar.toggle()}>
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+        </button>
+      </header>
+      <SidebarComponent
+        width={270}
+        ref={(Sidebar) => (sidebar = Sidebar)}
         showBackdrop={true}
         closeOnDocumentClick={true}
         created={toggleSidebar}
         type="over"
-        >
-            <NavBar handleClick={toggleSidebar}/>
-        </SidebarComponent>
-        
+      >
+        <NavItems handleClick={toggleSidebar} />
+        {/* <NavBar handleClick={toggleSidebar}/> */}
+      </SidebarComponent>
     </div>
-  )
-}
+  );
+};
 
-export default MobileSidebar
+export default MobileSidebar;
