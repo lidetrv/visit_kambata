7e858bc4da19cf41b368a33ce730492b5cfe8d08
Lidetu Tadesse <lidetutadesse6@gmail.com>
2025-11-28 23:58:06 +0300

    home4

--- app/routes/root/Homepage.tsx
+++ app/routes/root/Homepage.tsx
@@ -285,20 +285,6 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
           <div className="grid grid-cols-1 gap-9 md:grid-cols-2 lg:grid-cols-3">
             <div className="rounded-lg bg-white p-6 ring-8 ring-gray-900/5">
               <div className="mb-4 flex size-12 items-center justify-center rounded-xl bg-blue-100">
-                {/* <svg
-          className="inline-block size-6 text-blue-600"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          ></path>
-        </svg> */}
                 <svg
                   className="inline-block size-6 text-blue-600"
                   fill="none"
